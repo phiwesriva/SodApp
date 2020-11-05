bef567a81403cf4de014707c1f02c9ba089ee2d7
phiwesriva <phiwes@icloud.com>
2020-11-05 09:43:21 +0200

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,4 @@
-export interface Products {
+/*export interface Products {
  /* {
     name: 'Dress',
     price: 799,
@@ -13,11 +13,40 @@ export interface Products {
     name: 'Bottom',
     price: 299,
     description: 'A great phone with one of the best cameras'
-  },*/
+  },
   name: string;
   price: number;
   description: string;
-}
+}*/
+
+
+
+
+export const products = [
+  {
+    name: 'Dress',
+    price: 799,
+    /*Size : 'S' 'M' 'L',*/
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Top',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Bottom',
+    price: 299,
+    description: 'A great phone with one of the best cameras'
+  },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
 
 
 /*
--- src/app/tops/tops.component.ts
+++ src/app/tops/tops.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Products } from '../products';
-
+import { products } from '../products';
 
 @Component({
   selector: 'app-tops',
