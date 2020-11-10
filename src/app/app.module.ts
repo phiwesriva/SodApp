200264d8aa1078faf63a3790b7523112aa7ead82
phiwesriva <phiwes@icloud.com>
2020-11-10 11:51:00 +0200

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { InMemoryDataService } from './in-memory-data.service';
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { SearchComponent } from './search/search.component';
 
 
 
@@ -86,6 +87,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     ShippingComponent,
     DressesComponent,
     HeroSearchComponent,
+    SearchComponent,
     
     
   ],
